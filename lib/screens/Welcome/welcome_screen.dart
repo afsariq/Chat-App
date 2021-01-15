dd8954cb488bae1c5917d08010f8865e608de67f
Lasindu Nuwanga <lasindunuwanga@gmail.com>
2021-01-16 00:23:28 +0530

    Welcome screen layout fixed

--- lib/screens/Welcome/welcome_screen.dart
+++ lib/screens/Welcome/welcome_screen.dart
@@ -2,115 +2,97 @@ import 'package:chat_App/screens/GetNumber/get_number_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   // final double textScaleFactor = MediaQuery.of(context).textScaleFactor;
+    // final double textScaleFactor = MediaQuery.of(context).textScaleFactor;
 
     return Scaffold(
-       backgroundColor: Colors.white,
-        body: ListView(
-         children: <Widget>[
-           Container(
-             height: MediaQuery.of(context).size.height*0.92,
-             color: Colors.white,
-
-              
-          child: Column(
-            children:<Widget>[
-                 Column(
-                       children:<Widget>[
-              Image.asset("Assets/Images/logo1.png",width: 300,),
-               Text(
-                "Take Privacy With you.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontFamily: 'Exo2',
-                 fontSize: 25,
-                 // fontSize: textScaleFactor * 25,
-                    //fontWeight:FontWeight.bold,
-                    color: Colors.blue[900],
-                ),
-              ),
-              SizedBox(height: 15),
-              Text(
-                "Be Yourself in every message",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 25,
-                    //fontSize: textScaleFactor * 32,
-                    color: Colors.blue[900],
-                ),
-              ),
-              SizedBox(height: 65),
-              GestureDetector(
-                    child: Text(
-                      "Terms & Conditions",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.grey[400],
-                        //fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    onTap: () async {
-                      const url = 'https://google.com';
-                      if (await canLaunch(url)) {
-                        await launch(url);
-                      } else {
-                        throw 'Could not launch $url';
-                      }
-                    }),
-              SizedBox(height: 15),
-              SizedBox(
-                width: 250,
-                child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => GetPhoneNumberScreen()));
-                    },
-                    child: Text(
-                      "Continue",
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
-                    ),
-                    color: Colors.blue[800],
-                ),
-              ),
-
-            ]
+      backgroundColor: Colors.white,
+      body: Column(
+        children: <Widget>[
+          Image.asset(
+            "Assets/Images/logo1.png",
+            width: 300,
           ),
-                  
-           
-        
-       
-          ]),
-     
-
-        ),
-        
-  Container(
-              height: MediaQuery.of(context).size.height*0.08,
-
-             
-             decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("Assets/Images/Capture.PNG"),
-              
-              fit: BoxFit.cover,
+          Expanded(
+            child: Text(
+              "Take Privacy With you.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'Exo2',
+                fontSize: 25,
+                // fontSize: textScaleFactor * 25,
+                //fontWeight:FontWeight.bold,
+                color: Colors.blue[900],
+              ),
             ),
           ),
-         
-          
+          // SizedBox(height: 15),
+          Expanded(
+            flex: 2,
+            child: Text(
+              "Be Yourself in every message",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 25,
+                //fontSize: textScaleFactor * 32,
+                color: Colors.blue[900],
+              ),
+            ),
           ),
-           ]  ),
-        );
+          // SizedBox(height: 65),
+          GestureDetector(
+              child: Text(
+                "Terms & Conditions",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.grey[400],
+                  //fontWeight: FontWeight.bold,
+                ),
+              ),
+              onTap: () async {
+                const url = 'https://google.com';
+                if (await canLaunch(url)) {
+                  await launch(url);
+                } else {
+                  throw 'Could not launch $url';
+                }
+              }),
+          SizedBox(
+            width: MediaQuery.of(context).size.width - 20,
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => GetPhoneNumberScreen()));
+              },
+              child: Text(
+                "Continue",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+              color: Colors.blue[800],
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.08,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("Assets/Images/Capture.PNG"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
