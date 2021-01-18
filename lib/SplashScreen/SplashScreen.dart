f177584c1e9826b2e25eab618ce0958fa409c408
afsariq <afsariqbal07@gmail.com>
2021-01-18 09:38:33 +0530

    Merge branch 'main' of https://github.com/afsariq/Chat-App into main
    # Conflicts:
    #	pubspec.yaml

--- lib/SplashScreen/SplashScreen.dart
+++ lib/SplashScreen/SplashScreen.dart
@@ -5,7 +5,6 @@ import 'package:chat_App/screens/Welcome/welcome_screen.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class SplashScreen extends StatefulWidget {
-  
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
@@ -13,36 +12,33 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-   
     super.initState();
-    
-  
-    
-    Timer(Duration(seconds:3), ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>WelcomeScreen())));
- 
-     }
+
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => WelcomeScreen())));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-       body: Column(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        
-        children:<Widget> [
-
-          Image.asset("Assets/Images/machanlogo.jpeg",width: 180,height: 180,),
-          SizedBox(height:50),
-          SpinKitPulse(
-            color: Colors.blue,
-
-
+        children: <Widget>[
+          Image.asset(
+            "Assets/Images/logonew.png",
+            width: 180,
+            height: 180,
           ),
-          
-
-        ],),
-      
-      
+          SizedBox(height: 50),
+          SpinKitFoldingCube(
+            color: Colors.blue,
+          ),
+        ],
+      ),
     );
   }
 }
